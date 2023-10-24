1962260da0cbcd7835d2fffc18b432de5b38898b
david.leiton <>
2023-10-23 19:17:02 -0600

    Base eslint & prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-};
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: none,
+        jsxBracketSameLine: false,
+        bracketSpacing: true,
+        endOfLine: auto,
+        jsxSingleQuote: false,
+        semi: false
+      }
+    ]
+  }
+}
