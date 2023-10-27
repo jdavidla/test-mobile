9867b1baf6aebc1e9c8defd77ead19b030bb6867
david.leiton <>
2023-10-27 10:11:36 -0600

    added envs config source

--- src/shared/common/config.ts
+++ src/shared/common/config.ts
@@ -1,3 +1,5 @@
 import config from 'react-native-config'
 
 export const { RN_APP_CONFIG } = config
+
+// Envs config was build based on: https://medium.com/@sathishkcontact/managing-multiple-environments-in-react-native-android-ios-scripts-for-different-builds-ea4c5bff6782
