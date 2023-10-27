0d48869bfab98ed10b517936375e5e88aee521d6
david.leiton <>
2023-10-27 11:03:35 -0600

    Added splash screen for ios

--- App.tsx
+++ App.tsx
@@ -5,17 +5,21 @@
  * @format
  */
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView, useColorScheme } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { Colors } from 'react-native/Libraries/NewAppScreen'
 import { RN_APP_CONFIG } from './src/shared/common/config'
+import SplashScreen from 'react-native-splash-screen'
 import NavigationSwitch from './src/navigation/navigation-switch.component'
 
 function App(): JSX.Element {
-  console.log('Reading from env: ', RN_APP_CONFIG)
-  const isDarkMode = useColorScheme() === 'dark'
+  useEffect(() => {
+    console.log('Reading from env:', RN_APP_CONFIG)
+    SplashScreen.hide()
+  }, [])
 
+  const isDarkMode = useColorScheme() === 'dark'
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     flex: 1
