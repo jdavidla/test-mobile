fdaa83510cfbe5637bf11374263b01d27832adb4
david.leiton <>
2023-11-01 18:52:55 -0600

    Firebase base config & fixes on android build variants

--- ios/DavidTesting/AppDelegate.mm
+++ ios/DavidTesting/AppDelegate.mm
@@ -1,5 +1,5 @@
 #import "AppDelegate.h"
-
+#import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 #import "RNSplashScreen.h"
 
@@ -7,6 +7,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   self.moduleName = @"DavidTesting";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
