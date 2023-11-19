86174b51ada70b50ba01ec5171886bb34f819d34
David Leiton <david.leiton@firstfactory.com>
2023-11-19 15:44:45 -0600

    Added logout to profile

--- src/modules/profile/profile.component.tsx
+++ src/modules/profile/profile.component.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/auth.context'
+import { View, Text, Button } from 'react-native'
 
 const Profile = ({ route }) => {
+  const { logOut } = useContext(AuthContext)
+
   return (
     <View style={{ flex: 1 }}>
       <Text>Profile</Text>
       <Text>Route</Text>
       <Text>{JSON.stringify(route)}</Text>
+      <Button title="Log out" onPress={logOut} />
     </View>
   )
 }
